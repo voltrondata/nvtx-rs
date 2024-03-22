2b80f1a018b391a9477259cb34e8d81788b7b509
Will Killian <willk@voltrondata.com>
2024-03-22 19:24:34 -0400

    Add more examples (#4)
    Signed-off-by: Will Killian <willk@voltrondata.com>

---
+++ examples/range_start_end.rs
@@ -0,0 +1,47 @@
+use std::{thread, time};
+
+use nvtx_rs::nvtx;
+
+fn main() {
+    // we must hold ranges with a proper name
+    // _ will not work since drop() is called immediately
+    let app = nvtx::range_start(
+        nvtx::AttributeBuilder::default()
+            .color(nvtx::colors::salmon)
+            .message("Start 🦀")
+            .build(),
+    );
+    thread::sleep(time::Duration::from_millis(5));
+    for i in 10..=20 {
+        {
+            let iter = nvtx::range_start(
+                nvtx::AttributeBuilder::default()
+                    .color(nvtx::colors::cornflowerblue)
+                    .message(format!("Iteration Number {}", i))
+                    .payload(i)
+                    .build(),
+            );
+            for j in 1..=i {
+                {
+                    let inner = nvtx::range_start(
+                        nvtx::AttributeBuilder::default()
+                            .color(nvtx::colors::beige)
+                            .payload(j)
+                            .message("Inner")
+                            .build(),
+                    );
+                    thread::sleep(time::Duration::from_millis(10));
+                    nvtx::range_end(inner);
+                    if j == i / 2 {
+                        nvtx::range_end(iter);
+                    }
+                }
+                thread::sleep(time::Duration::from_millis(5));
+            }
+        }
+        thread::sleep(time::Duration::from_millis(10));
+        if i == 15 {
+            nvtx::range_end(app);
+        }
+    }
+}
