e1796b392b56163ce13aae38ee089b898285ac19
Will Killian <willk@voltrondata.com>
2024-03-26 13:27:14 -0400

    Add ability to name current thread; introduce behind feature (#14)
    * Add ability to name current thread; introduce behind feature
    * Update deny.toml
    
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- examples/domain_threads.rs
+++ examples/domain_threads.rs
@@ -1,37 +1,28 @@
 use std::{
-    ffi::{c_long, CString},
     thread::{self, sleep},
     time::Duration,
 };
 
-pub fn gettid() -> u32 {
-    const SYS_GETTID: c_long = 186;
-    extern "C" {
-        fn syscall(num: c_long, ...) -> c_long;
-    }
-    unsafe { syscall(SYS_GETTID) as u32 }
-}
-
 fn main() {
     let domain = nvtx::Domain::new("Domain");
     let d = &domain;
-    nvtx::name_thread(gettid(), CString::new("Main Thread").unwrap());
+    nvtx::name_current_thread("Main Thread");
 
     thread::scope(|s| {
         let r = d.range("Start on main thread");
         let t1 = s.spawn(move || {
-            nvtx::name_thread(gettid(), CString::new("Fork 1").unwrap());
+            nvtx::name_current_thread("Fork 1");
             sleep(Duration::from_millis(10));
             drop(r)
         });
         let t2 = s.spawn(move || {
-            nvtx::name_thread(gettid(), CString::new("Fork 2").unwrap());
+            nvtx::name_current_thread("Fork 2");
             let r = d.range("Start on Fork 2");
             sleep(Duration::from_millis(20));
             r
         });
         let t3 = s.spawn(move || {
-            nvtx::name_thread(gettid(), CString::new("Fork 3").unwrap());
+            nvtx::name_current_thread("Fork 3");
             let _r = d.range("Start and end on Fork 3");
             sleep(Duration::from_millis(30))
         });
--- examples/threads.rs
+++ examples/threads.rs
@@ -1,33 +1,24 @@
 use std::{
-    ffi::{c_long, CString},
     thread::{self, sleep},
     time::Duration,
 };
 
-pub fn gettid() -> u32 {
-    const SYS_GETTID: c_long = 186;
-    extern "C" {
-        fn syscall(num: c_long, ...) -> c_long;
-    }
-    unsafe { syscall(SYS_GETTID) as u32 }
-}
-
 fn main() {
-    nvtx::name_thread(gettid(), CString::new("Main Thread").unwrap());
+    nvtx::name_current_thread("Main Thread");
     let r = nvtx::Range::new("Start on main thread");
     let t1 = thread::spawn(move || {
-        nvtx::name_thread(gettid(), CString::new("Fork 1").unwrap());
+        nvtx::name_current_thread("Fork 1");
         sleep(Duration::from_millis(10));
         drop(r)
     });
     let t2 = thread::spawn(move || {
-        nvtx::name_thread(gettid(), CString::new("Fork 2").unwrap());
+        nvtx::name_current_thread("Fork 2");
         let r = nvtx::Range::new("Start on Fork 2");
         sleep(Duration::from_millis(20));
         r
     });
     let t3 = thread::spawn(move || {
-        nvtx::name_thread(gettid(), CString::new("Fork 3").unwrap());
+        nvtx::name_current_thread("Fork 3");
         let _r = nvtx::Range::new("Start and end on Fork 3");
         sleep(Duration::from_millis(30))
     });
